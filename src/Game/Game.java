ef94f9de319a0d44c8a1d106544b20b575c96ec4
Allan <allanfelipedk@gmail.com>
2024-01-18 19:30:32 -0300

    Game Looping

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -8,6 +8,9 @@ import javax.swing.JFrame;
 public class Game extends Canvas implements Runnable{
 
     private JFrame frame;
+    private Thread thread;
+
+    private boolean isRunning = true;
 
     private final int WIDTH = 160;
     private final int HEIGHT = 120;
@@ -15,7 +18,11 @@ public class Game extends Canvas implements Runnable{
 
 
     public Game(){
-        this.setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+        setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+        initFrame();
+    }
+
+    private void initFrame() {
         frame = new JFrame("graphics with java");
         frame.add(this);
         frame.setResizable(false);
@@ -25,11 +32,51 @@ public class Game extends Canvas implements Runnable{
         frame.setVisible(true);
     }
 
+    public synchronized void start(){
+        thread = new Thread(this);
+        isRunning = true;
+        thread.start();
+    }
+    public synchronized void stop(){
+
+    }
+
+
     public static void main(String[] args) throws Exception {
         Game game = new Game();
+        game.start();
+    }
+
+    public void tick(){
+
+    }
+    public void render(){
+
     }
 
     public void run(){
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        int frames = 0;
+        double timer = System.currentTimeMillis();
+        while (isRunning) {
+            long now = System.nanoTime();
+            delta+= (now - lastTime) / ns;
+            lastTime = now; 
+            if(delta >= 1){
+                tick();
+                render();
+                frames++;
+                delta--;
+            }
 
+            if(System.currentTimeMillis() - timer >= 1000){
+                System.out.println("FPS: " + frames);
+                frames = 0;
+                timer+= 1000;
+            }
+        }
     }
 }
